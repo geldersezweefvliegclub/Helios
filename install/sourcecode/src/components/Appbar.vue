58f4ca3e8a4946557f5212c599a3bc885feabc3d
Staijn1 <stein@jnkr.eu>
2020-12-30 12:47:39 +0100

    Router toegevoegd in installatie webpagina, met eerste pagina's

---
+++ install/sourcecode/src/components/Appbar.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-app-bar
+      app
+      color="primary"
+      light
+  >
+    <v-app-bar-title>
+      <router-link to="/" class="white--text">Installatie</router-link>
+    </v-app-bar-title>
+    <v-spacer></v-spacer>
+
+    <v-btn text>
+      <router-link to="/docs" class="white--text">Docs</router-link>
+    </v-btn>
+    <v-btn text>
+      <router-link to="/sha1" class="white--text">SHA1</router-link>
+    </v-btn>
+    <v-btn text>
+      <router-link to="/install" class="white--text">Installeren</router-link>
+    </v-btn>
+  </v-app-bar>
+</template>
+
+<script>
+export default {
+  name: "Appbar"
+}
+</script>
+
+<style lang="scss">
+.v-btn__content, .v-app-bar-title__content {
+  a {
+    text-decoration: none !important;
+  }
+}
+</style>
---
+++ install/sourcecode/src/views/Install.vue
@@ -0,0 +1,139 @@
+<template>
+  <v-container>
+    <v-stepper non-linear>
+      <v-stepper-header>
+        <v-stepper-step
+            editable
+            step="1"
+        >
+          Database account aanmaken
+        </v-stepper-step>
+
+        <v-divider></v-divider>
+
+        <v-stepper-step
+            editable
+            step="2"
+        >
+          Database aanmaken
+        </v-stepper-step>
+
+        <v-divider></v-divider>
+
+        <v-stepper-step
+            step="3"
+            editable
+        >
+          Tabellen aanmaken
+        </v-stepper-step>
+      </v-stepper-header>
+
+      <v-stepper-items>
+        <v-stepper-content step="1">
+          <v-card
+              class="mb-12"
+          >
+            <v-card-text>
+              <v-row class="d-flex justify-center">
+                <v-col
+                    cols="3">
+                  <v-text-field
+                      v-model="databaseAccountGegevens.databaseHost"
+                      type="text"
+                      label="Database server"
+                  ></v-text-field>
+                  <v-text-field
+                      v-model="databaseAccountGegevens.databaseNaam"
+                      type="text"
+                      label="Databasenaam"
+                  ></v-text-field>
+                  <v-text-field
+                      v-model="databaseAccountGegevens.databaseGebruiker"
+                      type="text"
+                      label="Gebruikersnaam"
+                  ></v-text-field>
+                  <v-text-field
+                      v-model="databaseAccountGegevens.databaseWachtwoord"
+                      type="password"
+                      label="Wachtwoord"
+                  ></v-text-field>
+                  <v-text-field
+                      v-model="databaseAccountGegevens.databaseWachtwoord"
+                      type="password"
+                      label="Wachtwoord herhalen"
+                  ></v-text-field>
+                </v-col>
+              </v-row>
+            </v-card-text>
+          </v-card>
+
+          <v-btn
+              color="primary"
+              @click="e1 = 2"
+          >
+            Continue
+          </v-btn>
+
+          <v-btn text>
+            Cancel
+          </v-btn>
+        </v-stepper-content>
+
+        <v-stepper-content step="2">
+          <v-card
+              class="mb-12"
+              color="grey lighten-1"
+          >
+            <v-card-text>Database aanmaken (alleen checkbox)</v-card-text>
+          </v-card>
+
+          <v-btn
+              color="primary"
+              @click="e1 = 3"
+          >
+            Continue
+          </v-btn>
+
+          <v-btn text>
+            Cancel
+          </v-btn>
+        </v-stepper-content>
+
+        <v-stepper-content step="3">
+          <v-card
+              class="mb-12"
+              color="grey lighten-1"
+          >
+            <v-card-text>Aangeven welke tabellen en dan knop voor aanmaken</v-card-text>
+          </v-card>
+
+          <v-btn
+              color="primary"
+              @click="e1 = 1"
+          >
+            Continue
+          </v-btn>
+
+          <v-btn text>
+            Cancel
+          </v-btn>
+        </v-stepper-content>
+      </v-stepper-items>
+    </v-stepper>
+  </v-container>
+</template>
+
+<script>
+export default {
+  data: function () {
+    return {
+      databaseAccountGegevens: {
+        databaseHost: undefined,
+        databaseNaam: undefined,
+        databaseGebruiker: undefined,
+        databaseWachtwoord: undefined,
+      }
+    }
+  }
+}
+</script>
