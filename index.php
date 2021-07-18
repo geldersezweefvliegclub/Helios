f8a4252bb259bce54b86485e339f6de620d773c9
richard <richard@jnkr.eu>
2021-07-18 10:11:03 +0200

    CORS probleem opgelost

--- index.php
+++ index.php
@@ -2,8 +2,6 @@
 
     // Allow from any origin
     if (isset($_SERVER['HTTP_ORIGIN'])) {
-      // Decide if the origin in $_SERVER['HTTP_ORIGIN'] is one
-      // you want to allow, and if so:
       header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
       header('Access-Control-Allow-Credentials: true');
       header('Access-Control-Max-Age: 86400');    // cache for 1 day
@@ -13,8 +11,7 @@
   if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
 
       if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
-          // may also be using PUT, PATCH, HEAD etc
-          header("Access-Control-Allow-Methods: GET, POST, OPTIONS");         
+          header("Access-Control-Allow-Methods: GET, POST, PATCH, DELETE, OPTIONS");         
 
       if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
           header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
