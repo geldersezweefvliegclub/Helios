e067c9c7f353beb2592af29cdd7c13f6d1c8fc8e
rjnkr <richard@jnkr.eu>
2023-11-08 12:36:23 +0100

    Eerste versie meldingen

---
+++ lib/class.Meldingen.inc.php
@@ -0,0 +1,549 @@
+<?php
+
+class Meldingen extends Helios
+{
+    function __construct()
+    {
+        parent::__construct();
+        $this->dbTable = "oper_meldingen";
+        $this->dbView = "meldingen_view";
+        $this->Naam = "Meldingen";
+    }
+
+    /*
+    Aanmaken van de database tabel. Indien FILLDATA == true, dan worden er ook voorbeeld records toegevoegd
+    */
+    function CreateTable($FillData = null)
+    {
+        $l = MaakObject('Login');
+        if ($l->isInstaller() == false)
+            throw new Exception("401;Geen installer;");
+
+        if (parent::bestaatTabel())
+            throw new Exception("405;Tabel bestaat al;");
+
+        $query = sprintf ("
+			CREATE TABLE `%s` (
+				`ID` mediumint  UNSIGNED NOT NULL AUTO_INCREMENT,
+				`DATUM` datetime  NOT NULL default CURRENT_TIMESTAMP,
+                `VLIEGTUIG_ID` mediumint UNSIGNED NULL,
+                `ROLLEND_ID` mediumint UNSIGNED NULL,
+				`TITEL` varchar(250) DEFAULT NULL,
+				`OMSCHRIJVING` text DEFAULT NULL,
+				`CATEGORIE_ID` mediumint UNSIGNED NULL,
+				`STATUS_ID` mediumint UNSIGNED NULL,
+				
+                `MELDER_ID` mediumint UNSIGNED NULL,
+                `TECHNICUS_ID` mediumint UNSIGNED NULL,
+                `AFGETEKEND_ID` mediumint UNSIGNED NULL,
+                            
+				`VERWIJDERD` tinyint UNSIGNED NOT NULL DEFAULT '0',
+				`LAATSTE_AANPASSING` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+				
+				CONSTRAINT ID_PK PRIMARY KEY (ID),
+					INDEX (`VLIEGTUIG_ID`),
+					INDEX (`STATUS_ID`),
+					INDEX (`DATUM`),
+					INDEX (`VERWIJDERD`),
+
+					FOREIGN KEY (ROLLEND_ID) REFERENCES ref_types(ID),
+					FOREIGN KEY (CATEGORIE_ID) REFERENCES ref_types(ID),
+					FOREIGN KEY (STATUS_ID) REFERENCES ref_types(ID),
+					FOREIGN KEY (VLIEGTUIG_ID) REFERENCES ref_vliegtuigen(ID),
+					FOREIGN KEY (MELDER_ID) REFERENCES ref_leden(ID),
+					FOREIGN KEY (TECHNICUS_ID) REFERENCES ref_leden(ID),
+					FOREIGN KEY (AFGETEKEND_ID) REFERENCES ref_leden(ID)
+			)", $this->dbTable);
+        parent::DbUitvoeren($query);
+    }
+
+    /*
+    Maak database views, als view al bestaat wordt deze overschreven
+    */
+    function CreateViews()
+    {
+        $l = MaakObject('Login');
+        if ($l->isInstaller() == false)
+            throw new Exception("401;Geen installer;");
+
+        $query = "CREATE VIEW `%s` AS
+		SELECT 
+            m.*,
+			`tr`.`OMSCHRIJVING` AS `ROLLEND`,
+			`ts`.`OMSCHRIJVING` AS `STATUS`,
+			`ts`.`CODE` AS `STATUS_CODE`,
+			`tc`.`OMSCHRIJVING` AS `CATEGORIE`,
+			`tc`.`CODE` AS `CATEGORIE_CODE`,
+			`lm`.`NAAM` AS `MELDER`,
+			`lt`.`NAAM` AS `TECHNICUS`,
+			`la`.`NAAM` AS `AFGETEKEND`,
+			CONCAT(IFNULL(`v`.`REGISTRATIE`,''),' (',IFNULL(`v`.`CALLSIGN`,''),')') AS `REG_CALL`
+			
+		FROM
+			`%s` `m`
+			LEFT JOIN `ref_types` `tr` ON (`m`.`ROLLEND_ID` = `tr`.`ID`)
+			LEFT JOIN `ref_types` `ts` ON (`m`.`STATUS_ID` = `ts`.`ID`)
+			LEFT JOIN `ref_types` `tc` ON (`m`.`CATEGORIE_ID` = `tc`.`ID`)
+			LEFT JOIN `ref_leden` `lm` ON (`m`.`MELDER_ID` = `lm`.`ID`)
+			LEFT JOIN `ref_leden` `lt` ON (`m`.`TECHNICUS_ID` = `lt`.`ID`)
+			LEFT JOIN `ref_leden` `la` ON (`m`.`AFGETEKEND_ID` = `lt`.`ID`)
+			LEFT JOIN `ref_vliegtuigen` `v`  ON (`m`.`VLIEGTUIG_ID` = `v`.`ID`)
+		WHERE
+			`m`.`VERWIJDERD` = %d 
+		ORDER BY `v`.`VOLGORDE`, `m`.`DATUM` DESC;";
+
+        parent::DbUitvoeren("DROP VIEW IF EXISTS meldingen_view");
+        parent::DbUitvoeren(sprintf($query, "meldingen_view", $this->dbTable, 0));
+
+        parent::DbUitvoeren("DROP VIEW IF EXISTS verwijderd_meldingen_view");
+        parent::DbUitvoeren(sprintf($query, "verwijderd_meldingen_view", $this->dbTable, 1));
+    }
+
+    /*
+    Haal een enkel record op uit de database
+    */
+    function GetObject($ID)
+    {
+        $functie = "Meldingen.GetObject";
+        Debug(__FILE__, __LINE__, sprintf("%s(%s)", $functie, $ID));
+
+        if ($ID == null)
+            throw new Exception("406;Geen ID in aanroep;");
+
+        $conditie = array();
+        $conditie['ID'] = isINT($ID, "ID");
+
+        $obj = parent::GetSingleObject($conditie);
+        Debug(__FILE__, __LINE__, print_r($obj, true));
+
+        if ($obj == null)
+            throw new Exception(sprintf("404;Record niet gevonden (%s, '%s');", $this->Naam, json_encode($conditie)));
+
+        $obj = $this->RecordToOutput($obj);
+        return $obj;
+    }
+
+    /*
+	Haal een dataset op met records als een array uit de database.
+	*/
+    function GetObjects($params)
+    {
+        global $app_settings;
+
+        $functie = "Meldingen.GetObjects";
+        Debug(__FILE__, __LINE__, sprintf("%s(%s)", $functie, print_r($params, true)));
+
+
+        $where = ' WHERE 1=1 ';
+        $orderby = "";
+        $alleenLaatsteAanpassing = false;
+        $hash = null;
+        $limit = 1000;	 // standaard max 1000 records
+        $start = -1;
+        $velden = "*";
+        $in = "";
+        $alleenVerwijderd = false;
+        $query_params = array();
+
+        foreach ($params as $key => $value)
+        {
+            switch ($key)
+            {
+                case "ID" :
+                {
+                    $id = isINT($value, "ID");
+                    $where .= " AND ID=?";
+                    array_push($query_params, $id);
+
+                    Debug(__FILE__, __LINE__, sprintf("%s: ID='%s'", $functie, $id));
+                    break;
+                }
+                case "VERWIJDERD" :
+                {
+                    $alleenVerwijderd = isBOOL($value, "VERWIJDERD");
+                    Debug(__FILE__, __LINE__, sprintf("%s: VERWIJDERD='%s'", $functie, $alleenVerwijderd));
+                    break;
+                }
+                case "LAATSTE_AANPASSING" :
+                {
+                    $alleenLaatsteAanpassing = isBOOL($value, "LAATSTE_AANPASSING");
+
+                    $alleenLaatsteAanpassing = ($alleenLaatsteAanpassing === 0) ? false : true;
+                    Debug(__FILE__, __LINE__, sprintf("%s: LAATSTE_AANPASSING='%s'", $functie, $alleenLaatsteAanpassing));
+                    break;
+                }
+                case "HASH" :
+                {
+                    $hash = $value;
+                    Debug(__FILE__, __LINE__, sprintf("%s: HASH='%s'", $functie, $hash));
+                    break;
+                }
+                case "VELDEN" :
+                {
+                    if (strpos($value,';') !== false)
+                        throw new Exception("405;VELDEN is onjuist;");
+
+                    $velden = strtoupper($value);
+                    Debug(__FILE__, __LINE__, sprintf("%s: VELDEN='%s'", $functie, $velden));
+                    break;
+                }
+                case "SORT" :
+                {
+                    if (strpos(strtoupper($value),'UPDATE') !== false)
+                        throw new Exception("405;SORT is onjuist;");
+
+                    if (strpos(strtoupper($value),'DELETE') !== false)
+                        throw new Exception("405;SORT is onjuist;");
+
+                    if (strpos($value,';') !== false)
+                        throw new Exception("405;SORT is onjuist;");
+
+                    $orderby = sprintf(" ORDER BY %s ", $value);
+                    Debug(__FILE__, __LINE__, sprintf("%s: SORT='%s'", $functie, $value));
+                    break;
+                }
+                case "START" :
+                {
+                    $s = isINT($value, "START");
+
+                    if ($s < 0)
+                        throw new Exception("405;START groter of gelijk zijn dan 0;");
+
+                    $start = $s;
+
+                    Debug(__FILE__, __LINE__, sprintf("%s: START='%s'", $functie, $start));
+                    break;
+                }
+                case "MAX" :
+                {
+                    $max = isINT($value, "MAX");
+
+                    if ($max > 0)
+                    {
+                        $limit = $max;
+                        Debug(__FILE__, __LINE__, sprintf("%s: LIMIT='%s'", $functie, $limit));
+                    }
+                    break;
+                }
+                case "BEGIN_DATUM" :
+                {
+                    $beginDatum = isDATE($value, "BEGIN_DATUM");
+
+                    $where .= " AND DATE(DATUM) >= ? ";
+                    array_push($query_params, $beginDatum);
+
+                    Debug(__FILE__, __LINE__, sprintf("%s: BEGIN_DATUM='%s'", $functie, $beginDatum));
+                    break;
+                }
+                case "EIND_DATUM" :
+                {
+                    $eindDatum = isDATE($value, "EIND_DATUM");
+
+                    $where .= " AND DATE(DATUM) <= ? ";
+                    array_push($query_params, $eindDatum);
+
+                    Debug(__FILE__, __LINE__, sprintf("%s: EIND_DATUM='%s'", $functie, $eindDatum));
+                    break;
+                }
+                case "VLIEGTUIG_ID" :
+                {
+                    isCSV($value, "VLIEGTUIG_ID");
+                    $where .= sprintf(" AND VLIEGTUIG_ID IN(%s)", trim($value));
+
+                    Debug(__FILE__, __LINE__, sprintf("%s: VLIEGTUIG_ID='%s'", $functie, trim($value)));
+                    break;
+                }
+                case "LID_ID" :
+                {
+                    isCSV($value, "LID_ID");
+                    $where .= sprintf(" AND LID_ID IN(%s)", trim($value));
+
+                    Debug(__FILE__, __LINE__, sprintf("%s: LID_ID='%s'", $functie, trim($value)));
+                    break;
+                }
+                case "MELDER_ID" :
+                {
+                    isCSV($value, "LID_ID");
+                    $where .= sprintf(" AND MELDER_ID IN(%s)", trim($value));
+
+                    Debug(__FILE__, __LINE__, sprintf("%s: LID_ID='%s'", $functie, trim($value)));
+                    break;
+                }
+                case "TECHNICUS_ID" :
+                {
+                    isCSV($value, "LID_ID");
+                    $where .= sprintf(" AND TECHNICUS_ID IN(%s)", trim($value));
+
+                    Debug(__FILE__, __LINE__, sprintf("%s: TECHNICUS_ID='%s'", $functie, trim($value)));
+                    break;
+                }
+                case "STATUS_ID" :
+                {
+                    isCSV($value, "STATUS_ID");
+                    $where .= sprintf(" AND STATUS_ID IN(%s)", trim($value));
+
+                    Debug(__FILE__, __LINE__, sprintf("%s: TECHNICUS_ID='%s'", $functie, trim($value)));
+                    break;
+                }
+                default:
+                {
+                    throw new Exception(sprintf("405;%s is een onjuiste parameter;", $key));
+                }
+            }
+        }
+
+        if (!array_key_exists('LID_ID', $params))
+            $where .= " AND LID_ID is null ";
+
+        $query = "
+			SELECT 
+				%s
+			FROM
+				`####dmeldingen_view` " . $where . $orderby;
+        $query = str_replace("####", ($alleenVerwijderd ? "verwijderd_" : "") , $query);
+
+        $retVal = array();
+
+        $retVal['totaal'] = $this->Count($query, $query_params);		// totaal aantal of record in de database
+        $retVal['laatste_aanpassing']=  $this->LaatsteAanpassing($query, $query_params);
+        Debug(__FILE__, __LINE__, sprintf("TOTAAL=%d, LAATSTE_AANPASSING=%s", $retVal['totaal'], $retVal['laatste_aanpassing']));
+
+        if ($alleenLaatsteAanpassing)
+        {
+            $retVal['dataset'] = null;
+            return $retVal;
+        }
+        else
+        {
+            if ($limit > 0)
+            {
+                if ($start < 0)				// Is niet meegegeven, dus start op 0
+                    $start = 0;
+
+                $query .= sprintf(" LIMIT %d , %d ", $start, $limit);
+            }
+            $rquery = sprintf($query, $velden);
+            parent::DbOpvraag($rquery, $query_params);
+            $retVal['dataset'] = parent::DbData();
+
+            $retVal['hash'] = hash("crc32", json_encode($retVal));
+            Debug(__FILE__, __LINE__, sprintf("HASH=%s", $retVal['hash']));
+
+            if ($retVal['hash'] == $hash)
+                throw new Exception(sprintf("%d;Dataset ongewijzigd;", $app_settings['dataNotModified']));
+
+            for ($i=0 ; $i < count($retVal['dataset']) ; $i++)
+            {
+                $retVal['dataset'][$i] = $this->RecordToOutput($retVal['dataset'][$i]);
+            }
+            return $retVal;
+        }
+        return null;  // Hier komen we nooit :-)
+    }
+
+    /*
+	Toevoegen van een record. Het is niet noodzakelijk om alle velden op te nemen in het verzoek
+	*/
+    function AddObject($melding)
+    {
+        global $app_settings;
+
+        $functie = "Meldingen.AddObject";
+        Debug(__FILE__, __LINE__, sprintf("%s(%s)", $functie, print_r($melding, true)));
+
+        $l = MaakObject('Login');
+
+        if ($melding == null)
+            throw new Exception("406;Melding data moet ingevuld zijn;");
+
+        if (!isset($melding['DATUM']))
+            throw new Exception("406;DATUM is verplicht;");
+
+        if (!isset($melding['TITEL']))
+            throw new Exception("406;TITEL is verplicht;");
+
+        if (!isset($melding['OMSCHRIJVING']))
+            throw new Exception("406;OMSCHRIJVING is verplicht;");
+
+        if (!isset($melding['VLIEGTUIG_ID']) && isset($melding['ROLLEND_ID']))
+            throw new Exception("406;VLIEGTUIG_ID of ROLLEND_ID is verplicht;");
+
+        if (!isset($melding['MELDER_ID']))
+            throw new Exception("406;MELDER_ID is verplicht;");
+
+        if (!isset($melding['STATUS_ID']))
+            throw new Exception("406;STATUS_ID is verplicht;");
+
+        if (!isset($melding['CATEGORIE_ID']))
+            throw new Exception("406;CATEGORIE_ID is verplicht;");
+
+        // Neem data over uit aanvraag
+        $record = $this->RequestToRecord($melding);
+        $id = parent::DbToevoegen($record);
+
+        Debug(__FILE__, __LINE__, sprintf("Melding toegevoegd id=%d", $id));
+        return $id;
+    }
+
+    /*
+    Update van een bestaand record. Het is niet noodzakelijk om alle velden op te nemen in het verzoek
+    */
+    function UpdateObject($melding)
+    {
+        global $app_settings;
+
+        $functie = "Meldingen.UpdateObject";
+        Debug(__FILE__, __LINE__, sprintf("%s(%s)", $functie, print_r($melding, true)));
+
+        // schrijven mag alleen door beheerder / CIMT
+        $l = MaakObject('Login');
+
+        if (!$l->isBeheerder() && !$l->isCIMT())
+            throw new Exception("401;Geen rechten;");
+
+        if ($melding == null)
+            throw new Exception("406;Melding data moet ingevuld zijn;");
+
+        if (!array_key_exists('ID', $melding))
+            throw new Exception("406;ID moet ingevuld zijn;");
+
+        $id = isINT($melding['ID'], "ID");
+
+        // Neem data over uit aanvraag
+        $d = $this->RequestToRecord($melding);
+
+        parent::DbAanpassen($id, $d);
+        if (parent::NumRows() === 0)
+            throw new Exception(sprintf("404;Record niet gevonden (%s, '%s');", $this->Naam, $id));
+
+        return $this->GetObject($id);
+    }
+
+    /*
+    Markeer een record in de database als verwijderd. Het record wordt niet fysiek verwijderd om er een link kan zijn naar andere tabellen.
+    Het veld VERWIJDERD wordt op "1" gezet.
+    */
+    function VerwijderObject($id, $verificatie = true)
+    {
+        $functie = "Meldingen.VerwijderObject";
+        Debug(__FILE__, __LINE__, sprintf("%s('%s', %s)", $functie, $id, (($verificatie === false) ? "False" :  $verificatie)));
+
+        // schrijven mag alleen door beheerder / CIMT
+        $l = MaakObject('Login');
+
+        if (!$l->isBeheerder() && !$l->isCIMT())
+            throw new Exception("401;Geen rechten;");
+
+        if ($id == null)
+            throw new Exception("406;Geen ID in aanroep;");
+
+        isCSV($id, "ID");
+        parent::MarkeerAlsVerwijderd($id, $verificatie);
+    }
+
+    /*
+    Herstel van een verwijderd record
+    */
+    function HerstelObject($id)
+    {
+        $functie = "Meldingen.HerstelObject";
+        Debug(__FILE__, __LINE__, sprintf("%s('%s')", $functie, $id));
+
+        // schrijven mag alleen door beheerder / CIMT
+        $l = MaakObject('Login');
+
+        if (!$l->isBeheerder())
+            throw new Exception("401;Geen rechten;");
+
+        if ($id == null)
+            throw new Exception("406;Geen ID in aanroep;");
+
+        isCSV($id, "ID");
+        parent::HerstelVerwijderd($id);
+    }
+
+    /*
+	Copieer data van request naar velden van het record
+	*/
+    function RequestToRecord($input)
+    {
+        $record = array();
+
+        $field = 'ROLLEND_ID';
+        if (array_key_exists($field, $input))
+            $record[$field] = isINT($input[$field], $field, true, "Types");
+
+        $field = 'STATUS_ID';
+        if (array_key_exists($field, $input))
+            $record[$field] = isINT($input[$field], $field, false, "Types");
+
+        $field = 'CATEGORIE_ID';
+        if (array_key_exists($field, $input))
+            $record[$field] = isINT($input[$field], $field, false, "Types");
+
+        $field = 'MELDER_ID';
+        if (array_key_exists($field, $input))
+            $record[$field] = isINT($input[$field], $field, false, "Leden");
+
+        $field = 'TECHNICUS_ID';
+        if (array_key_exists($field, $input))
+            $record[$field] = isINT($input[$field], $field, true, "Leden");
+
+        $field = 'AFGETEKEND_ID';
+        if (array_key_exists($field, $input))
+            $record[$field] = isINT($input[$field], $field, true, "Leden");
+
+        $field = 'VLIEGTUIG_ID';
+        if (array_key_exists($field, $input))
+            $record[$field] = isINT($input[$field], $field, true, "Vliegtuigen");
+
+        $field = 'TITEL';
+        if (array_key_exists($field, $input))
+            $record[$field] = $input[$field];
+
+        $field = 'OMSCHRIJVING';
+        if (array_key_exists($field, $input))
+            $record[$field] = $input[$field];
+
+        return $record;
+    }
+
+    /*
+    Converteer integers en booleans voor correcte output
+    */
+    function RecordToOutput($record)
+    {
+        $retVal = $record;
+
+        // vermengvuldigen met 1 converteer naar integer
+        if (isset($record['ID']))
+            $retVal['ID']  = $record['ID'] * 1;
+
+        if (isset($record['ROLLEND_ID']))
+            $retVal['ROLLEND_ID']  = $record['ROLLEND_ID'] * 1;
+
+        if (isset($record['CATEGORIE_ID']))
+            $retVal['CATEGORIE_ID']  = $record['CATEGORIE_ID'] * 1;
+
+        if (isset($record['STATUS_ID']))
+            $retVal['STATUS_ID']  = $record['STATUS_ID'] * 1;
+
+        if (isset($record['VLIEGTUIG_ID']))
+            $retVal['VLIEGTUIG_ID']  = $record['VLIEGTUIG_ID'] * 1;
+
+        if (isset($record['MELDER_ID']))
+            $retVal['MELDER_ID']  = $record['MELDER_ID'] * 1;
+
+        if (isset($record['TECHNICUS_ID']))
+            $retVal['TECHNICUS_ID']  = $record['TECHNICUS_ID'] * 1;
+
+        if (isset($record['AFGETEKEND_ID']))
+            $retVal['AFGETEKEND_ID']  = $record['AFGETEKEND_ID'] * 1;
+
+        // booleans
+        if (isset($record['VERWIJDERD']))
+            $retVal['VERWIJDERD']  = $record['VERWIJDERD'] == "1" ? true : false;
+
+        return $retVal;
+    }
+}
---
+++ routes/route.Meldingen.php
@@ -0,0 +1,223 @@
+<?php
+
+// Gebruik het slim framework
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Factory\AppFactory;
+
+/*
+Aanmaken van de database tabel. Indien FILLDATA == true, dan worden er ook voorbeeld records toegevoegd 
+*/
+$app->post(url_base() . 'Meldingen/CreateTable', function (Request $request, Response $response, $args) {
+    $obj = MaakObject("Meldingen");
+    try
+    {
+        $params = $request->getQueryParams();
+        $fill = (isset($params['FILLDATA'])) ? $params['FILLDATA'] : null;
+
+        $obj->CreateTable($fill);   // Hier staat de logica voor deze functie
+        return $response->withStatus(intval(201));
+    }
+    catch(Exception $exception)
+    {
+        Debug(__FILE__, __LINE__, "/Meldingen/CreateTable: " .$exception);
+
+        list($dummy, $exceptionMsg) = explode(": ", $exception);
+        list($httpStatus, $message) = explode(";", $exceptionMsg);  // onze eigen formaat van een exceptie
+
+        header("X-Error-Message: $message", true, intval($httpStatus));
+        header("Content-Type: text/plain");
+        die;
+    }
+});
+
+/*
+Maak database views, als view al bestaat wordt deze overschreven
+*/
+$app->post(url_base() . 'Meldingen/CreateViews', function (Request $request, Response $response, $args) {
+    $obj = MaakObject("Meldingen");
+    try
+    {
+        $obj->CreateViews();    // Hier staat de logica voor deze functie
+        return $response->withStatus(intval(201));
+    }
+    catch(Exception $exception)
+    {
+        Debug(__FILE__, __LINE__, "/Meldingen/CreateViews: " .$exception);
+
+        list($dummy, $exceptionMsg) = explode(": ", $exception);
+        list($httpStatus, $message) = explode(";", $exceptionMsg);  // onze eigen formaat van een exceptie
+
+        header("X-Error-Message: $message", true, intval($httpStatus));
+        header("Content-Type: text/plain");
+        die;
+    }
+});
+
+/*
+Haal een enkel record op uit de database
+*/
+$app->get(url_base() . 'Meldingen/GetObject', function (Request $request, Response $response, $args) {
+    $obj = MaakObject("Meldingen");
+    try
+    {
+        $params = $request->getQueryParams();
+        $id = (isset($params['ID'])) ? $params['ID'] : null;
+
+        $l = $obj->GetObject($id);  // Hier staat de logica voor deze functie
+        if ($l === null)
+        {
+            header("X-Error-Message: Geen data", true, 404);
+            header("Content-Type: text/plain");
+            die;
+        }
+
+        $response->getBody()->write(json_encode($l));
+        return $response->withHeader('Content-Type', 'application/json');
+    }
+    catch(Exception $exception)
+    {
+        Debug(__FILE__, __LINE__, "/Meldingen/GetObject: " .$exception);
+
+        list($dummy, $exceptionMsg) = explode(": ", $exception);
+        list($httpStatus, $message) = explode(";", $exceptionMsg);  // onze eigen formaat van een exceptie
+
+        header("X-Error-Message: $message", true, intval($httpStatus));
+        header("Content-Type: text/plain");
+        die;
+    }
+});
+
+/*
+Haal een dataset op met records als een array uit de database. 
+*/
+$app->get(url_base() . 'Meldingen/GetObjects', function (Request $request, Response $response, $args) {
+    $obj = MaakObject("Meldingen");
+    try
+    {
+        $parameters = $request->getQueryParams();
+        $v = $obj->GetObjects($parameters);     // Hier staat de logica voor deze functie
+
+        $response->getBody()->write(json_encode($v));
+        return $response->withHeader('Content-Type', 'application/json');
+    }
+    catch(Exception $exception)
+    {
+        Debug(__FILE__, __LINE__, "/Meldingen/GetObjects: " .$exception);
+
+        list($dummy, $exceptionMsg) = explode(": ", $exception);
+        list($httpStatus, $message) = explode(";", $exceptionMsg);   // onze eigen formaat van een exceptie
+
+        header("X-Error-Message: $message", true, intval($httpStatus));
+        header("Content-Type: text/plain");
+        die;
+    }
+});
+
+/*
+Markeer een record in de database als verwijderd. Het record wordt niet fysiek verwijderd om er een link kan zijn naar andere tabellen.
+Het veld VERWIJDERD wordt op "1" gezet.
+*/
+$app->delete(url_base() . 'Meldingen/DeleteObject', function (Request $request, Response $response, $args) {
+    $obj = MaakObject("Meldingen");
+    try
+    {
+        $params = $request->getQueryParams();
+        $id = (isset($params['ID'])) ? $params['ID'] : null;
+        $verificatie = (isset($params['VERIFICATIE'])) ? $params['VERIFICATIE'] : null;
+
+        $obj->VerwijderObject($id, $verificatie);     // Hier staat de logica voor deze functie
+        return $response->withStatus(intval(204));
+    }
+    catch(Exception $exception)
+    {
+        Debug(__FILE__, __LINE__, "/Meldingen/DeleteObject: " .$exception);
+
+        list($dummy, $exceptionMsg) = explode(": ", $exception);
+        list($httpStatus, $message) = explode(";", $exceptionMsg);  // onze eigen formaat van een exceptie
+
+        header("X-Error-Message: $message", true, intval($httpStatus));
+        header("Content-Type: text/plain");
+        die;
+    }
+});
+
+/*
+Haal een record terug dat verwijderd is . Het record was gelukkig niet fysiek verwijderd om er een link kan zijn naar andere tabellen.
+Het veld VERWIJDERD wordt terug op "0" gezet.
+*/
+$app->patch(url_base() . 'Meldingen/RestoreObject', function (Request $request, Response $response, $args) {
+    $obj = MaakObject("Meldingen");
+    try
+    {
+        $params = $request->getQueryParams();
+        $id = (isset($params['ID'])) ? $params['ID'] : null;
+
+        $record = $obj->HerstelObject($id);     // Hier staat de logica voor deze functie
+        return $response->withStatus(intval(202));
+    }
+    catch(Exception $exception)
+    {
+        Debug(__FILE__, __LINE__, "/Meldingen/RestoreObject: " .$exception);
+
+        list($dummy, $exceptionMsg) = explode(": ", $exception);
+        list($httpStatus, $message) = explode(";", $exceptionMsg);  // onze eigen formaat van een exceptie
+
+        header("X-Error-Message: $message", true, intval($httpStatus));
+        header("Content-Type: text/plain");
+        die;
+    }
+});
+
+/*
+Aanmaken van een record. Het is niet noodzakelijk om alle velden op te nemen in het verzoek
+*/
+$app->post(url_base() . 'Meldingen/SaveObject', function (Request $request, Response $response, $args) {
+    $obj = MaakObject("Meldingen");
+    try
+    {
+        $data = json_decode($request->getBody(), true);
+
+        $v = $obj->AddObject($data);   // Hier staat de logica voor deze functie
+        $response->getBody()->write(json_encode($v));
+        return $response->withHeader('Content-Type', 'application/json');
+    }
+    catch(Exception $exception)
+    {
+        Debug(__FILE__, __LINE__, "/Meldingen/SaveObject: " .$exception);
+
+        list($dummy, $exceptionMsg) = explode(": ", $exception);
+        list($httpStatus, $message) = explode(";", $exceptionMsg);  // onze eigen formaat van een exceptie
+
+        header("X-Error-Message: $message", true, intval($httpStatus));
+        header("Content-Type: text/plain");
+        die;
+    }
+});
+
+/*
+Aanpassen van een record. Het is niet noodzakelijk om alle velden op te nemen in het verzoek
+*/
+$app->put(url_base() . 'Meldingen/SaveObject', function (Request $request, Response $response, $args) {
+    $obj = MaakObject("Meldingen");
+    try
+    {
+        $data = json_decode($request->getBody(), true);
+
+        $v = $obj->UpdateObject($data);   // Hier staat de logica voor deze functie
+        $response->getBody()->write(json_encode($v));
+        return $response->withHeader('Content-Type', 'application/json');
+    }
+    catch(Exception $exception)
+    {
+        Debug(__FILE__, __LINE__, "/Meldingen/SaveObject: " .$exception);
+
+        list($dummy, $exceptionMsg) = explode(": ", $exception);
+        list($httpStatus, $message) = explode(";", $exceptionMsg);  // onze eigen formaat van een exceptie
+
+        header("X-Error-Message: $message", true, intval($httpStatus));
+        header("Content-Type: text/plain");
+        die;
+    }
+});
+?>
