d2d6095f2345780d9d0ba340ad40d6965c9a5915
Staijn1 <stein@jnkr.eu>
2024-01-28 14:52:30 +0100

    Add swagger UI

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,31 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import {INestApplication, Logger} from "@nestjs/common";
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+function setupSwagger(app: INestApplication, swaggerUrl: string) {
+  const swaggerConfig = new DocumentBuilder()
+      .setTitle('Helios API')
+      .setDescription('De Helios API')
+      .setVersion('1.0')
+      .addBearerAuth()
+      .build();
+
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup(swaggerUrl, app, document);
+}
+
 
 async function bootstrap() {
+  const port = 3333;
+
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  setupSwagger(app, 'docs');
+
+  app.enableCors();
+  await app.listen(port);
+
+  Logger.log(`🚀 Application is running on: http://localhost:${port}/`);
 }
 bootstrap();
