111a00e4ccee8c1a57eebfcc59d53543b2d8ff12
Staijn1 <stein@jnkr.eu>
2024-02-04 17:22:23 +0100

    Remove default App stuff

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,19 +1,16 @@
-import {Module} from '@nestjs/common';
-import {AppController} from './app.controller';
-import {AppService} from './app.service';
-import {ConfigModule} from '@nestjs/config';
-import {TypeOrmModule} from "@nestjs/typeorm";
-import {TypeOrmConfigService} from "./typeorm/typeorm.service";
-import {TypesModule} from "./modules/Types/types.module";
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TypeOrmConfigService } from './typeorm/typeorm.service';
+import { TypesModule } from './modules/Types/types.module';
 
 @Module({
-    imports: [
-        ConfigModule.forRoot({isGlobal: true}),
-        TypeOrmModule.forRootAsync({useClass: TypeOrmConfigService}),
-        TypesModule
-    ],
-    controllers: [AppController],
-    providers: [AppService, TypeOrmConfigService],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({ useClass: TypeOrmConfigService }),
+    TypesModule,
+  ],
+  controllers: [],
+  providers: [TypeOrmConfigService],
 })
-export class AppModule {
-}
+export class AppModule {}
