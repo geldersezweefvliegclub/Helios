1157889c05949e03d66cdee8f32f1a063cdf0315
Staijn1 <stein@jnkr.eu>
2024-02-04 17:27:14 +0100

    Implement IHeliosService

--- src/helpers/IHeliosService.ts
+++ src/helpers/IHeliosService.ts
@@ -2,7 +2,4 @@ import { GetObjectsResponse } from './GetObjectsResponse';
 
 export interface IHeliosService<T> {
     GetObjects(): Promise<GetObjectsResponse<T>>
-
-    CreateViews(): Promise<void>
-
 }
--- src/modules/Types/service/types.service.ts
+++ src/modules/Types/service/types.service.ts
@@ -3,21 +3,25 @@ import { FindManyOptions, Repository } from 'typeorm';
 import { TypeEntity } from '../entities/Type.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { createHash } from 'crypto';
+import { IHeliosService } from '../../../helpers/IHeliosService';
+import { GetObjectsResponse } from 'src/helpers/GetObjectsResponse';
 
 @Injectable()
-export class TypesService {
-  constructor(@InjectRepository(TypeEntity) private readonly typesRepository: Repository<TypeEntity>) {}
+export class TypesService implements IHeliosService<TypeEntity> {
+  constructor(@InjectRepository(TypeEntity) private readonly typesRepository: Repository<TypeEntity>) {
+  }
+
 
   async getObject(id: number) {
     return this.typesRepository.findOne({ where: { ID: id } });
   }
 
-  async getObjects(filter: FilterCriteria): Promise<TypeEntity[]> {
+  async getObjects(filter: FilterCriteria): Promise<GetObjectsResponse<TypeEntity>> {
     const findOptions: FindManyOptions<TypeEntity> = {
       where: filter,
     };
     const dataset = await this.typesRepository.find(findOptions);
-    const hash = createHash('md5').update(JSON.stringify(data)).digest('hex');
+    const hash = createHash('md5').update(JSON.stringify(dataset)).digest('hex');
 
     return {
       totaal: dataset.length,
