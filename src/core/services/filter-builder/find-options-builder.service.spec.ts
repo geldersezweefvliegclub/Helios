9a051508220069c7cc544a6b947f8b56af06b79f
Staijn1 <stein@jnkr.eu>
2024-02-08 20:11:16 +0100

    Write tests for find options builder & make required fixes

--- src/core/services/filter-builder/find-options-builder.service.spec.ts
+++ src/core/services/filter-builder/find-options-builder.service.spec.ts
@@ -16,4 +16,18 @@ describe('FindOptionsBuilder', () => {
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  it('should chain multiple AND conditions', () => {
+    service.and({ id: 1 }).and({ name: 'John' });
+    expect(service.findOptions.where).toEqual([{ id: 1, name: 'John' }]);
+  });
+
+  it('should throw when adding an AND to a non-existing OR condition', () => {
+    expect(() => service.and({ id: 1 }, 1)).toThrow();
+  })
+
+  it('should chain multiple OR conditions', () => {
+    service.or({ id: 1 }).or({ name: 'John' });
+    expect(service.findOptions.where).toEqual([{ id: 1 }, { name: 'John' }]);
+  });
 });
--- src/core/services/filter-builder/find-options-builder.service.ts
+++ src/core/services/filter-builder/find-options-builder.service.ts
@@ -1,17 +1,22 @@
 import { Injectable } from '@nestjs/common';
 import { FindManyOptions, ObjectLiteral } from 'typeorm';
 import { FindOptionsWhere } from 'typeorm/find-options/FindOptionsWhere';
-import { IHeliosEntity } from '../../DTO/IHeliosEntity';
+import { InvalidArgumentException } from '../../helpers/exceptions/InvalidArgumentException';
 
 @Injectable()
 export class FindOptionsBuilder<Entity extends ObjectLiteral> {
-  private findOptions: FindManyOptions<Entity> = {
+  get findOptions(): FindManyOptions<Entity> {
+    return this._findOptions;
+  }
+  private _findOptions: FindManyOptions<Entity> = {
     where: [],
   };
 
   public and(condition: FindOptionsWhere<Entity>, index: number = 0) {
-    const currentWhere = this.findOptions.where as FindOptionsWhere<Entity>[];
+    const currentWhere = this._findOptions.where as FindOptionsWhere<Entity>[];
     const currentCondition = currentWhere[index] as FindOptionsWhere<Entity>;
+
+    if (index < 0 || (index >= currentWhere.length && index != 0)) throw new InvalidArgumentException('Invalid index to build AND filter')
 
     currentWhere[index] = {
       ...currentCondition,
@@ -22,7 +27,7 @@ export class FindOptionsBuilder<Entity extends ObjectLiteral> {
   }
 
   public or(condition: FindOptionsWhere<Entity>) {
-    (this.findOptions.where as FindOptionsWhere<Entity>[]).push(condition);
+    (this._findOptions.where as FindOptionsWhere<Entity>[]).push(condition);
     return this;
   }
 }
