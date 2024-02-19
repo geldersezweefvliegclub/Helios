9a312275d9b1d42745ac51b2cb4a747562a962df
Staijn1 <stein@jnkr.eu>
2024-02-19 19:05:23 +0100

    Fix wrong data in progressiekaart

--- src/modules/Progressie/Progressie.module.ts
+++ src/modules/Progressie/Progressie.module.ts
@@ -1,16 +1,15 @@
-
 import { Module } from '@nestjs/common';
 import { ProgressieController } from './controller/Progressie.controller';
 import { ProgressieService } from './service/Progressie.service';
 import { ProgressieEntity } from './entities/Progressie.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuditEntity } from '../../core/entities/Audit.entity';
-import {CompetentiesEntity} from "../Competenties/entities/Competenties.entity";
-import {ProgressieViewEntity} from "./entities/ProgressieView.entity";
+import { ProgressieViewEntity } from './entities/ProgressieView.entity';
+import { CompetentiesViewEntity } from '../Competenties/entities/CompetentiesView.entity';
 
 @Module({
     imports: [
-        TypeOrmModule.forFeature([ProgressieEntity, AuditEntity, CompetentiesEntity, ProgressieViewEntity]),
+        TypeOrmModule.forFeature([ProgressieEntity, AuditEntity, CompetentiesViewEntity, ProgressieViewEntity]),
     ],
     controllers: [ProgressieController],
     providers: [ProgressieService],
--- src/modules/Progressie/service/Progressie.service.ts
+++ src/modules/Progressie/service/Progressie.service.ts
@@ -1,34 +1,34 @@
-import {Injectable} from '@nestjs/common';
-import {Repository} from 'typeorm';
-import {ProgressieEntity} from '../entities/Progressie.entity';
-import {InjectRepository} from '@nestjs/typeorm';
-import {IHeliosService} from '../../../core/base/IHelios.service';
-import {AuditEntity} from '../../../core/entities/Audit.entity';
-import {ProgressieKaartFilterDTO} from "../DTO/ProgressieKaartFilterDTO";
-import {CompetentiesEntity} from "../../Competenties/entities/Competenties.entity";
-import {GetObjectsResponse} from "../../../core/base/GetObjectsResponse";
-import {ProgressiekaartDTO} from "../DTO/ProgressiekaartDTO";
-import {ProgressieViewEntity} from '../entities/ProgressieView.entity';
+import { Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { ProgressieEntity } from '../entities/Progressie.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { IHeliosService } from '../../../core/base/IHelios.service';
+import { AuditEntity } from '../../../core/entities/Audit.entity';
+import { ProgressieKaartFilterDTO } from '../DTO/ProgressieKaartFilterDTO';
+import { GetObjectsResponse } from '../../../core/base/GetObjectsResponse';
+import { ProgressiekaartDTO } from '../DTO/ProgressiekaartDTO';
+import { ProgressieViewEntity } from '../entities/ProgressieView.entity';
+import { CompetentiesViewEntity } from '../../Competenties/entities/CompetentiesView.entity';
 
 @Injectable()
 export class ProgressieService extends IHeliosService<ProgressieEntity, ProgressieViewEntity> {
-  constructor(
-    @InjectRepository(ProgressieEntity) protected readonly repository: Repository<ProgressieEntity>,
-    @InjectRepository(ProgressieViewEntity) protected readonly viewRepository: Repository<ProgressieViewEntity>,
-    @InjectRepository(CompetentiesEntity) protected readonly competentiesRepository: Repository<CompetentiesEntity>,
-    @InjectRepository(AuditEntity) protected readonly auditRepository: Repository<AuditEntity>
-  ) {
-    super(repository, viewRepository, auditRepository);
-  }
+    constructor(
+        @InjectRepository(ProgressieEntity) protected readonly repository: Repository<ProgressieEntity>,
+        @InjectRepository(ProgressieViewEntity) protected readonly viewRepository: Repository<ProgressieViewEntity>,
+        @InjectRepository(CompetentiesViewEntity) protected readonly competentieViewRepository: Repository<CompetentiesViewEntity>,
+        @InjectRepository(AuditEntity) protected readonly auditRepository: Repository<AuditEntity>
+    ) {
+        super(repository, viewRepository, auditRepository);
+    }
 
     async getProgressiekaart(filter: ProgressieKaartFilterDTO): Promise<GetObjectsResponse<ProgressiekaartDTO>> {
-        const datasetRaw = await this.competentiesRepository.createQueryBuilder('c')
-            .select('c.BLOK as BLOK, c.BLOK_ID as BLOK_ID, c.DOCUMENTATIE as DOCUMENTATIE, p.GELDIG_TOT as GELDIG_TOT, c.ID as ID, p.INGEVOERD as INGEVOERD, p.INSTRUCTEUR_NAAM as INSTRUCTEUR_NAAM, p.LEERFASE as LEERFASE, c.LEERFASE_ID as LEERFASE_ID, c.ONDERWERP as ONDERWERP, p.OPMERKINGEN as OPMERKINGEN, p.ID as PROGRESSIE_ID, c.SCORE as SCORE')
+        const datasetRaw = await this.competentieViewRepository.createQueryBuilder('c')
+            .select('c.BLOK as BLOK, c.BLOK_ID as BLOK_ID, c.DOCUMENTATIE as DOCUMENTATIE, p.GELDIG_TOT as GELDIG_TOT, c.ID as ID, p.INGEVOERD as INGEVOERD, p.INSTRUCTEUR_NAAM as INSTRUCTEUR_NAAM, c.LEERFASE as LEERFASE, c.LEERFASE_ID as LEERFASE_ID, c.ONDERWERP as ONDERWERP, p.OPMERKINGEN as OPMERKINGEN, p.ID as PROGRESSIE_ID, p.SCORE as SCORE')
             .leftJoin(qb => {
                 return qb
                     .select('*')
                     .from(ProgressieViewEntity, 'p')
-                    .where('p.LID_ID = :lidId', { lidId: filter.LID_ID });
+                    .where('p.LID_ID = :lidId', {lidId: filter.LID_ID});
             }, 'p', 'c.ID = p.COMPETENTIE_ID')
             .orderBy({
                 'c.LEERFASE_ID': 'ASC',
