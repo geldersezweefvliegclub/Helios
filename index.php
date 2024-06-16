140fe9f6294890c9bed52484a6e8733dea9d5f97
rjnkr <richard@jnkr.eu>
2024-06-16 20:22:19 +0100

    X-Error-Message must be exposed in CORS calls

--- index.php
+++ index.php
@@ -15,6 +15,7 @@ if (isset($_SERVER['HTTP_ORIGIN']))
 {
     header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
     header('Access-Control-Allow-Credentials: true');
+    header('Access-Control-Expose-Headers: X-Error-Message');
     header('Access-Control-Max-Age: 86400');    // cache for 1 day
 }
 
